c3bf3360e271303cc528358677b951c1d6242124
Jeevan <shresthaj1986@gmail.com>
2025-03-14 20:11:26 +0545

    Updated content of terms and conditions

--- src/app/(root)/terms/page.tsx
+++ src/app/(root)/terms/page.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import data from '@/data.json';
 import { webUrl } from '@/utils/constant';
+import { CgNotes } from 'react-icons/cg';
 
 export const metadata = {
   title: 'Terms and Conditions | Recondition Hub',
@@ -22,12 +23,31 @@ export default function TermsAndConditions() {
   return (
     <div className="bg-gray-100 py-10">
       <div className="container">
-        <h1 className="mb-4 text-2xl font-bold">Terms and Conditions</h1>
-        <div className="space-y-4">
+        <h1 className="mb-4 inline-flex items-center gap-1 border-b-2 border-dashed border-secondary text-2xl font-bold text-secondary">
+          <CgNotes size={24} />
+          <span>Terms and Conditions</span>
+        </h1>
+        <p className="mb-4 font-semibold text-gray-600">
+          Welcome to Recondition Hub! By using our platform, you agree to the
+          following terms and conditions. Please read them carefully before
+          using the service.
+        </p>
+        <div className="space-y-6">
           {data?.terms?.map((section, index) => (
             <section key={index}>
-              <h2 className="text-lg font-semibold">{section.title}</h2>
-              <p className="text-sm text-gray-600">{section.content}</p>
+              <h2 className="text-lg font-bold">
+                {index + 1}. {section.title}
+              </h2>
+              <ul className="list-disc pl-9">
+                {section.content?.map((point, pointIndex) => (
+                  <li
+                    key={pointIndex}
+                    className="mt-1 font-semibold text-gray-600"
+                  >
+                    {point}
+                  </li>
+                ))}
+              </ul>
             </section>
           ))}
         </div>
